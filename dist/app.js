4cc7b5079783708d21ba2f5ded85554ee69e7f50
Nick Sheward <nickshwrd@gmail.com>
2023-10-02 15:53:08 +0100

    Pre merge commit

--- dist/app.js
+++ dist/app.js
@@ -14,6 +14,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 require("dotenv/config");
+const cors_1 = __importDefault(require("cors"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const passport_1 = __importDefault(require("passport"));
 const express_session_1 = __importDefault(require("express-session"));
@@ -38,7 +39,12 @@ app.use((0, express_session_1.default)({ secret: 'cats', resave: false, saveUnin
 app.use(passport_1.default.initialize());
 app.use(passport_1.default.session());
 app.use(express_1.default.json());
-app.use(express_1.default.static('public'));
+// NOTE URL for front end
+app.use((0, cors_1.default)({
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    methods: ['POST', 'GET', 'DELETE', 'PUT', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use('/', indexRoutes_1.default);
 // app.use('/user', userRoutes);
