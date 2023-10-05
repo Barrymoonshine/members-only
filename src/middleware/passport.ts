94ec763e9d2225e43fc5326c0d18980f933b16bb
Nick Sheward <nickshwrd@gmail.com>
2023-10-05 12:10:01 +0100

    Removed no longer needed callback from passport local strategy

--- src/middleware/passport.ts
+++ src/middleware/passport.ts
@@ -5,24 +5,21 @@ import User from '../models/user';
 import { Types } from 'mongoose';
 
 passport.use(
-  new LocalStrategy(
-    { passReqToCallback: true },
-    async (req, username: string, password: string, done) => {
-      try {
-        const user = await User.findOne({ username });
-        if (!user) {
-          return done(null, false);
-        }
-        const match = await bcrypt.compare(password, user.password);
-        if (!match) {
-          return done(null, false);
-        }
-        return done(null, user);
-      } catch (err) {
-        return done(err);
+  new LocalStrategy(async (username: string, password: string, done) => {
+    try {
+      const user = await User.findOne({ username });
+      if (!user) {
+        return done(null, false);
       }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false);
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
     }
-  )
+  })
 );
 
 // Property id does not exist on type user, any added so TS doesn't perform any type checking
